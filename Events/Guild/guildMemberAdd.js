2f4f37ccab97cc5548a9922d45fd9a1b158f98a4
Mark57449 <souowendel@gmail.com>
2023-04-25 03:11:35 -0300

    Criação da pasta e do evento de interação que recepciona os novos membros de servidores atribuindo um cargo.

---
+++ Events/Guild/guildMemberAdd.js
@@ -0,0 +1,24 @@
+import { EmbedBuilder } from '@discordjs/builders';
+// eslint-disable-next-line no-unused-vars
+import { GuildMember, Embed } from 'discord.js';
+
+export default {
+    name: 'guildMemberAdd',
+    async execute(member) {
+        // eslint-disable-next-line no-unused-vars
+        const [user, guild] = member;
+        const welcomeChannel = member.guild.channels.cache.get('1100292682061971456');
+        const welcomeMessage = `Bem-vindo <${member.id}> ao servidor!`;
+        const memberRole = '1100295395009105973';
+
+        const welcomeEmbed = new EmbedBuilder()
+        .setTitle('Novo Membro')
+        .setDescription(welcomeMessage)
+        .setColor(0x037821)
+        .addFields({ name: 'Total de Membros', value: `${guild.memberCount}` })
+        .setTimestamp();
+
+        welcomeChannel.send({ content: welcomeEmbed });
+        member.roles.add(memberRole);
+    },
+};
