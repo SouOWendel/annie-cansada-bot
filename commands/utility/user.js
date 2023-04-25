ad8d006daed84fe52e78e46c59d1f258ca407aed
Mark57449 <souowendel@gmail.com>
2023-04-25 02:57:34 -0300

    refactor: Pequena adequação para melhor leitura do código.

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -3,6 +3,7 @@ import { SlashCommandBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
 	.setName('user')
 	.setDescription('Provides information about the user.');
+
 export async function execute(interaction) {
 	// interaction.user is the object representing the User who ran the command
 	// interaction.member is the GuildMember object, which represents the user in the specific guild
