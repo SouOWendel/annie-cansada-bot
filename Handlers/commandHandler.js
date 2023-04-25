d26a201608a9ca8e8efa68a6b5e7364567ff47b5
Mark57449 <souowendel@gmail.com>
2023-04-25 03:06:36 -0300

    feat: Função de gerenciamento de comandos, algo que foi retirado do arquivo index.js e também reprogramado segundo as práticas de programação do kajdev (youtube).

---
+++ Handlers/commandHandler.js
@@ -0,0 +1,28 @@
+export async function loadCommands(client) {
+    // https://www.npmjs.com/package/ascii-table3
+    const { AsciiTable3 } = await import('ascii-table3');
+    const fs = await import('fs');
+    const table = new AsciiTable3().setHeading('Commands', 'Status');
+
+    const commandsArray = [];
+
+    const commandsFolder = fs.readdirSync('./Commands');
+    for (const folder of commandsFolder) {
+        const commandFiles = fs.readdirSync(`./Commands/${folder}`).filter((file) => file.endsWith('js'));
+
+        for (const file of commandFiles) {
+            const commandFile = await import(`../Commands/${folder}/${file}`);
+
+            client.commands.set(commandFile.data.name, commandFile);
+
+            commandsArray.push(commandFile.data.toJSON());
+
+            table.addRow(file, 'loaded');
+            continue;
+        }
+    }
+
+    client.application.commands.set(commandsArray);
+
+    return console.log(table.toString(), '\nLoaded Commands');
+}
