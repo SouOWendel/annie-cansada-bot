2b82d7073fa078d9ff4b2d26169d8e497cc2d9b8
Mark57449 <souowendel@gmail.com>
2023-04-25 03:04:16 -0300

    Readequação e refatoramento do código para melhor eficiência. Diversas alterações baseadas no desenvolvedor kajdev (youtube). No geral as funções agora são separadas em diversos arquivos de modo dinâmico, novos módulos foram adicionados diretamente do discord.js (partials) e instanciamento de variaveis do GatewayIntentBits e Partials.

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 /* eslint-disable brace-style */
-// Require the necessary discord.js classes
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits, Partials } from 'discord.js';
 import 'dotenv/config';
-import { readdirSync, statSync } from 'node:fs';
-import { join } from 'node:path';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { loadEvents } from './Handlers/eventHandler.js';
+import { loadCommands } from './Handlers/commandHandler.js';
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Guilds, GuildMembers, GuildMessages, GuildVoiceStates } = GatewayIntentBits;
+// eslint-disable-next-line no-unused-vars
+const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
+
+const client = new Client({
+	intents: [Guilds, GuildMembers, GuildMessages, GuildVoiceStates],
+	partials: [User, Message, GuildMember, ThreadMember],
+ });
 
 client.commands = new Collection();
-
-const foldersPath = join(__dirname, 'commands');
-const pastas = readdirSync(foldersPath).filter(file => statSync(path.join(foldersPath, file)).isDirectory());
-
-	for (const p of pastas) {
-		// Grab all the command files from the commands directory you created earlier
-		const commandsPath = join(foldersPath, p);
-		const commandFiles = readdirSync(commandsPath);
-		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-		for (const file of commandFiles) {
-			const comando = await import('./commands/' + p + '/' + file);
-			console.log('./commands/' + p + '/' + file);
-			if (comando.data && comando.execute) {
-				client.commands.set(comando.data.name, comando);
-				console.log('Comando atribuido com exito.')
-			} else {
-				console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
-			}
-		}
-	}
-
-// console.log(client.commands);
 
 // ----------------------------------------------------
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, async c => {
 	console.log(`Pronto! Logando como ${c.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.client.member) {
+		console.log('User is in voice channel!');
+	}
 
 	const command = client.commands.get(interaction.commandName);
 	// console.log(command);
@@ -57,12 +41,15 @@ client.on(Events.InteractionCreate, async interaction => {
 	} catch (error) {
 		console.error(error);
 		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.followUp({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
 		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			await interaction.reply({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
 		}
 	}
 });
 
 // Log in to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN).then(() => {
+	loadEvents(client);
+	loadCommands(client);
+});
