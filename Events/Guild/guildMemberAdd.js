dd77236b8e31d025de9f77c8854d0d23834389b6
Mark57449 <souowendel@gmail.com>
2023-04-26 19:31:59 -0300

    fix: Corrigida a função de recepção de membros novos no servidor.

--- Events/Guild/guildMemberAdd.js
+++ Events/Guild/guildMemberAdd.js
@@ -6,7 +6,7 @@ export default {
     name: 'guildMemberAdd',
     async execute(member) {
         // eslint-disable-next-line no-unused-vars
-        const [user, guild] = member;
+        const { user, guild } = member;
         const welcomeChannel = member.guild.channels.cache.get('1100292682061971456');
         const welcomeMessage = `Bem-vindo <${member.id}> ao servidor!`;
         const memberRole = '1100295395009105973';
@@ -18,7 +18,7 @@ export default {
         .addFields({ name: 'Total de Membros', value: `${guild.memberCount}` })
         .setTimestamp();
 
-        welcomeChannel.send({ content: welcomeEmbed });
+        welcomeChannel.send({ embeds: [welcomeEmbed] });
         member.roles.add(memberRole);
     },
 };
