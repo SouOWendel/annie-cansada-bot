b23f8261fc311596db5da6c0b18ad46bce3de34a
Mark57449 <souowendel@gmail.com>
2023-04-27 11:37:11 -0300

    Adição da função de atividade (activity) do bot ao iniciar.

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,10 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import { Client } from 'discord.js';
+import { ActivityType, Client } from 'discord.js';
 
 export default {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log(`${client.user.username} agora está online.`);
+        client.user.setActivity('Devilline HQ — Character', { type: ActivityType.Playing });
+        setTimeout(() => {
+            console.log(`\n${client.user.username} agora está online.`);
+            console.log('Status: Devilline HQ — Character.');
+        }, 100);
     },
 };
