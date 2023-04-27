131a699b066c8d1a98d56849d48ac6ed755db8ff
Mark57449 <souowendel@gmail.com>
2023-04-27 11:38:56 -0300

    Importação dos modulos de DisTube e Spotify Plugin. Além do instanciamento de um novo objeto DisTube (client.distube). Parametros de DisTube: emitNewSongOnly: true, leaveOnEmpty: true, leaveOnStop: true, emptyCooldown: 60, leaveOnFinish: false.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
 import 'dotenv/config';
 
+import { DisTube } from 'distube';
+import { SpotifyPlugin } from '@distube/spotify';
+
 import { loadEvents } from './Handlers/eventHandler.js';
 import { loadCommands } from './Handlers/commandHandler.js';
 
@@ -15,7 +18,21 @@ const client = new Client({
 	partials: [User, Message, GuildMember, ThreadMember],
  });
 
+// Sistema de Música YOUTUBE/SPOTIFY
+client.distube = new DisTube(client, {
+	emitNewSongOnly: true,
+	leaveOnEmpty: true,
+	leaveOnStop: true,
+	emptyCooldown: 60,
+	leaveOnFinish: false, /* Sair quando finalizar */
+	emitAddSongWhenCreatingQueue: false,
+	plugins: [new SpotifyPlugin()],
+});
+
+// Coleção de comandos
 client.commands = new Collection();
+
+export default client;
 
 loadEvents(client);
 
