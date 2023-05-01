ad21bedb0a93eb833d4ed2080100ec11165ad111
Mark57449 <souowendel@gmail.com>
2023-05-01 05:21:03 -0300

    Adaptação para mais de um bot e adicionando provaveis futuros recursos, além de suporte a atividade para o bot.

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,14 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import { ActivityType, Client } from 'discord.js';
+import { usagePercent } from 'cpu-stat';
+import { ExtractorPlugin } from 'distube';
 
 export default {
     name: 'ready',
     once: true,
     execute(client) {
-        client.user.setActivity('Devilline HQ — Character', { type: ActivityType.Playing });
-        setTimeout(() => {
+
+        if (client.user.username == 'Beatrice') client.user.setActivity('Re:Zero Waifu', { type: ActivityType.Playing });
+        else if (client.user.username == 'Annie Cansada') client.user.setActivity('Devilline HQ — Character', { type: ActivityType.Playing });
+
+        setTimeout(async () => {
             console.log(`\n${client.user.username} agora está online.`);
             console.log('Status: Devilline HQ — Character.');
-        }, 100);
+            // console.table({
+            //     'Time Stamp': new Date().getTime(),
+            //     'OS': 'aaaa',
+            //     'Browser': 'aaaa',
+            //     'Language': 'aaaa',
+            //   });
+        }, 200);
+
     },
 };
