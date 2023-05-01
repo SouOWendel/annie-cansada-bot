27d7f7002e0152b680b7119f8e53cffd727feb43
Mark57449 <souowendel@gmail.com>
2023-05-01 05:22:23 -0300

    feat: Criado arquivo de event PLAY_SONG.

---
+++ Events/Distube/playSong.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import { Client } from 'discord.js';
+import { Events, ExtractorPlugin } from 'distube';
+
+// https://www.appsloveworld.com/discordjs/100/26/discord-js-music-bot-not-working-distube-framework
+export default {
+    name: Events.PLAY_SONG,
+    distube: true,
+    execute(client) {
+        // const queue = client.distube.getQueue(client.id);
+        // if (queue.songs !== undefined) {
+        //     console.log(queue.songs.song.streamURL);
+        // }
+        // console.log(queue.songs);
+        // console.log(ExtractorPlugin.getStreamURL('https://www.youtube.com/watch?v=2vrwkXjdjEs'));
+    },
+};
