76868982709b5438349857fb95e7a481ea1e2fda
Mark57449 <souowendel@gmail.com>
2023-05-01 05:22:03 -0300

    Função para cooldown do bot sair do canal utilizando o event do DisTube chamado 'FINISH', após acabarem as músicas na queue.

---
+++ Events/Distube/emptyLeaveCooldown.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line no-unused-vars
+import { Client } from 'discord.js';
+import { Events } from 'distube';
+// https://www.appsloveworld.com/discordjs/100/26/discord-js-music-bot-not-working-distube-framework
+export default {
+    name: Events.FINISH,
+    distube: true,
+    execute(client) {
+        console.log('Player em espera...');
+        console.log('Servidor ID: ' + client.id);
+        // console.log(client.distube);
+
+        setTimeout(() => {
+            const queue = client.distube.getQueue(client.id);
+            // if (!client.distube.queue.guild.members.me.voice.channelId) {
+            //   return clearTimeout();
+            // }
+
+            if (queue !== undefined && queue.songs.length != 0) {
+                // console.log('clear');
+                // console.log('Tocando algo? ' + queue.duration);
+                // console.log('Musicas na playlist:' + queue.songs.length);
+
+                return clearTimeout();
+            } else if (queue == undefined || queue.songs.length == 0) {
+                // console.log('stop');
+                client.distube.voices.leave(client.id);
+            }
+          }, 120000);
+    },
+};
