12a34bf5a8bb1eb63d8fb586589cb99530048a35
Mark57449 <souowendel@gmail.com>
2023-09-08 01:37:59 -0300

    fix(events): correção da função que envia uma mensagem de boas vindas aos novos usuários do servidor

--- Events/Guild/guildMemberAdd.js
+++ Events/Guild/guildMemberAdd.js
@@ -3,22 +3,25 @@ import { EmbedBuilder } from '@discordjs/builders';
 import { GuildMember, Embed } from 'discord.js';
 
 export default {
-    name: 'guildMemberAdd',
-    async execute(member) {
-        // eslint-disable-next-line no-unused-vars
-        const { user, guild } = member;
-        const welcomeChannel = member.guild.channels.cache.get('1100292682061971456');
-        const welcomeMessage = `Bem-vindo <${member.id}> ao servidor!`;
-        const memberRole = '1100295395009105973';
+	name: 'guildMemberAdd',
+	async execute(member) {
+		// eslint-disable-next-line no-unused-vars
+		const { user, guild } = member;
+		console.log(member);
+		console.log(guild);
+		const welcomeChannel = await member.guild.channels.cache.get(guild.systemChannelId);
+		const welcomeMessage = `Bem-vindo <${member.user.username}> ao servidor!`;
+		const memberRole = undefined;
 
-        const welcomeEmbed = new EmbedBuilder()
-        .setTitle('Novo Membro')
-        .setDescription(welcomeMessage)
-        .setColor(0x037821)
-        .addFields({ name: 'Total de Membros', value: `${guild.memberCount}` })
-        .setTimestamp();
+		const welcomeEmbed = new EmbedBuilder()
+			.setTitle('Novo Membro')
+			.setDescription(welcomeMessage)
+			.setColor(0x037821)
+			.addFields({ name: 'Total de Membros', value: `${guild.memberCount}` })
+			.setTimestamp();
 
-        welcomeChannel.send({ embeds: [welcomeEmbed] });
-        member.roles.add(memberRole);
-    },
+		welcomeChannel.send({ embeds: [welcomeEmbed] });
+
+		if (memberRole) member.roles.add(memberRole);
+	},
 };
