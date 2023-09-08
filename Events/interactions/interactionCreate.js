f58dd019c37c6cc327c5f1103e7e5ca8c00ac44a
Mark57449 <souowendel@gmail.com>
2023-09-08 01:39:19 -0300

    perf(events): adição de console.time para medição do tempo de resposta de comandos do interaction

--- Events/interactions/interactionCreate.js
+++ Events/interactions/interactionCreate.js
@@ -1,25 +1,27 @@
 // import { CommandInteraction } from 'discord.js';
 
 export default {
-    name: 'interactionCreate',
-    async execute(interaction, client) {
-        if (!interaction.isChatInputCommand()) return;
+	name: 'interactionCreate',
+	async execute(interaction, client) {
+		console.time('Tempo para execução do comando');
+		if (!interaction.isChatInputCommand()) return;
 
-        const command = client.commands.get(interaction.commandName);
+		const command = client.commands.get(interaction.commandName);
 
-        if (!command) {
-            interaction.reply({ content: 'outdated command' });
-        }
+		if (!command) {
+			interaction.reply({ content: 'Comando inexistente.' });
+		}
 
-        try {
-            await command.execute(interaction, client);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
-            }
-        }
-    },
+		try {
+			await command.execute(interaction, client);
+			console.timeEnd('Tempo para execução do comando');
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'Houve um erro durante a execução do comando!', ephemeral: true });
+			}
+		}
+	},
 };
