55d8c11a283ffbcae7d4a0207e3ec77e79991fbe
Mark57449 <souowendel@gmail.com>
2023-09-08 01:34:16 -0300

    fix(events): correção da função que retira o bot do canal de voz quando estiver ocioso

--- Events/Distube/emptyLeaveCooldown.js
+++ Events/Distube/emptyLeaveCooldown.js
@@ -3,29 +3,30 @@ import { Client } from 'discord.js';
 import { Events } from 'distube';
 // https://www.appsloveworld.com/discordjs/100/26/discord-js-music-bot-not-working-distube-framework
 export default {
-    name: Events.FINISH,
-    distube: true,
-    execute(client) {
-        console.log('Player em espera...');
-        console.log('Servidor ID: ' + client.id);
-        // console.log(client.distube);
+	name: Events.DELETE_QUEUE,
+	distube: true,
+	execute(client) {
+		console.log('Player em espera...');
+		console.time('Tempo expirado');
+		console.log('Servidor ID: ' + client.id);
+		// console.log(client.distube);
 
-        setTimeout(() => {
-            const queue = client.distube.getQueue(client.id);
-            // if (!client.distube.queue.guild.members.me.voice.channelId) {
-            //   return clearTimeout();
-            // }
+		setTimeout(() => {
+			const queue = client.distube.getQueue(client.id);
+			// if (!client.distube.queue.guild.members.me.voice.channelId) {
+			//   return clearTimeout();
+			// }
 
-            if (queue !== undefined && queue.songs.length != 0) {
-                // console.log('clear');
-                // console.log('Tocando algo? ' + queue.duration);
-                // console.log('Musicas na playlist:' + queue.songs.length);
+			if (queue !== undefined && queue.songs.length != 0) {
+				// console.log('clear');
+				// console.log('Tocando algo? ' + queue.duration);
+				// console.log('Musicas na playlist:' + queue.songs.length);
 
-                return clearTimeout();
-            } else if (queue == undefined || queue.songs.length == 0) {
-                // console.log('stop');
-                client.distube.voices.leave(client.id);
-            }
-          }, 120000);
-    },
+				return clearTimeout();
+			} else if (queue == undefined || queue.songs.length == 0) {
+				client.distube.voices.leave(client.id);
+				console.timeEnd('Tempo expirado');
+			}
+		}, 240000);
+	},
 };
