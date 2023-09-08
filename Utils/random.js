3efc6caad236ab6ed59c6331c2effebd7bf8976c
Mark57449 <souowendel@gmail.com>
2023-09-08 01:43:12 -0300

    feat(utils): adição da feature de fatos aleatórios
    Essa função resgata aleatoriamente de acordo uma chance pré-configurada, através de parametros,
    fatos aleatórios para serem exibidos aonde for preciso

--- Utils/random.js
+++ Utils/random.js
@@ -1,10 +1,23 @@
+import { facts } from '../Data/dataFatos.js';
+
 // Um método simples que retorna um emoji aleatório. Quero adicionar tipos de emoções no futuro como parametro e ifs.
 export function getRandomEmoji() {
-    const emojiList = ['😭', '😄', '😌', '🤓', '😎', '😤', '🤖', '😶‍🌫️', '🌏', '📸', '💿', '👋', '🌊', '✨'];
-    return emojiList[Math.floor(Math.random() * emojiList.length)];
-  }
+	const emojiList = ['😭', '😄', '😌', '🤓', '😎', '😤', '🤖', '😶‍🌫️', '🌏', '📸', '💿', '👋', '🌊', '✨'];
+	return emojiList[Math.floor(Math.random() * emojiList.length)];
+}
 
-  // Retorna um número aleatório baseado na quantidade de números.
-  export function getRandomNumber(limit) {
-    return Math.floor(Math.random() * limit);
-  }
+// Retorna um número aleatório baseado na quantidade de números.
+export function getRandomNumber(limit) {
+	return Math.floor(Math.random() * limit);
+}
+
+// Método para retornar uma curiosidade aleatória.
+export function getRandomFact(probabilidade) {
+	const n = getRandomNumber(100);
+	const array_position = getRandomNumber(facts.length);
+
+	// Se a probabilidade é maior do que o número aleatório 'n'. Deve-se retornar um fato.
+
+	if (probabilidade >= n) return facts[array_position];
+	else return null;
+}
