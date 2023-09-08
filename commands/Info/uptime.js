5d41d47c5fff49d11967eb5775a10e6aea141ad2
Mark57449 <souowendel@gmail.com>
2023-09-08 01:19:29 -0300

    refactor(commands): personalização de interação com o client
    A descrição foi atualizada para ficar de acordo com a proposta da Annie

--- commands/Info/uptime.js
+++ commands/Info/uptime.js
@@ -3,22 +3,22 @@ import { SlashCommandBuilder, EmbedBuilder, Client } from 'discord.js';
 
 // https://old.discordjs.dev/#/docs/builders/main/class/SlashCommandBuilder?scrollTo=options
 export const data = new SlashCommandBuilder()
-    .setName('uptime')
-    .setDescription('uptime!');
+	.setName('uptime')
+	.setDescription('Cê descobre a quanto tempo eu tô sem dormir...');
 
 export async function execute(interaction) {
-    const days = Math.floor(interaction.client.uptime / 86400000);
-    const hours = Math.floor(interaction.client.uptime / 3600000) % 24;
-    const minutes = Math.floor(interaction.client.uptime / 60000) % 60;
-    const seconds = Math.floor(interaction.client.uptime / 1000) % 60;
+	const days = Math.floor(interaction.client.uptime / 86400000);
+	const hours = Math.floor(interaction.client.uptime / 3600000) % 24;
+	const minutes = Math.floor(interaction.client.uptime / 60000) % 60;
+	const seconds = Math.floor(interaction.client.uptime / 1000) % 60;
 
-    const embed = new EmbedBuilder()
-        .setName(`__${interaction.client.user.username}'s Uptime__`)
-        .setColor('Blue')
-        .setTimestamp()
-        .addFields(
-            { name: 'Uptime', value: ` \`${days}\` days, \`${hours}\` hours, \`${minutes}\` minutes and \`${seconds}\` seconds.` },
-        );
+	const embed = new EmbedBuilder()
+		.setTitle(`__${interaction.client.user.username}'s Uptime__`)
+		.setColor('Blue')
+		.setTimestamp()
+		.addFields(
+			{ name: 'Uptime', value: ` \`${days}\` days, \`${hours}\` hours, \`${minutes}\` minutes and \`${seconds}\` seconds.` },
+		);
 
-    interaction.reply({ embeds: [embed] });
+	interaction.reply({ embeds: [embed] });
 }
