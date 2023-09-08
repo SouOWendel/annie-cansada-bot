5b5253127e9488afde875e60c15cdb9ef844bb5d
Mark57449 <souowendel@gmail.com>
2023-09-08 01:48:06 -0300

    feat: leaveOnStop false, nsfw true, mudança do cookie do YouTube, collection e errorHandler
    Mudança da configuração do Distube para não sair quando acabar a reprodução, habilitado a exibição
    de vídeos do YouTube considerados NSFW, alteração do cookie do YouTube, carregamento do errorHandler
    e alteração do cookie do YouTube por um funcional

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { YtDlpPlugin } from '@distube/yt-dlp';
 
 import { loadEvents } from './Handlers/eventHandler.js';
 import { loadCommands } from './Handlers/commandHandler.js';
+import { loadErrorHandler } from './Handlers/errorHandler.js';
 
 // Create a new client instance
 const { Guilds, GuildMembers, GuildMessages, GuildVoiceStates } = GatewayIntentBits;
@@ -17,15 +18,15 @@ const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 const client = new Client({
 	intents: [Guilds, GuildMembers, GuildMessages, GuildVoiceStates],
 	partials: [User, Message, GuildMember, ThreadMember],
- });
+});
 
 // Sistema de Música YOUTUBE/SPOTIFY
 client.distube = new DisTube(client, {
 	emitNewSongOnly: false,
 	joinNewVoiceChannel: true,
 	leaveOnEmpty: true,
-	leaveOnStop: true,
-	nsfw: false,
+	leaveOnStop: false,
+	nsfw: true,
 	emptyCooldown: 10,
 	leaveOnFinish: false, /* Sair quando finalizar */
 	emitAddSongWhenCreatingQueue: false,
@@ -34,11 +35,14 @@ client.distube = new DisTube(client, {
 		new YtDlpPlugin({ update: true }),
 	],
 	streamType: StreamType.OPUS,
-	youtubeCookie: 'DEVICE_INFO=ChxOekU1TXpReU9EWXhPRGcwTmpVMk1qVXdNZz09ELfi0J4GGLfi0J4G; VISITOR_INFO1_LIVE=6bLeb7pBUG4; YSC=1zoSDo7XW88; PREF=f6=40000000&tz=America.Sao_Paulo&f7=150; HSID=Aul4dZA4xKn8DnEsT; SSID=A7AOek-oDr3YjHRAJ; APISID=kjCgcGMRjVDbVudk/A6D4USHQHsoO4UAJx; SAPISID=TA9nKw4_K1EqAc9-/AivJ-A3v3EIXXnqAL; __Secure-1PAPISID=TA9nKw4_K1EqAc9-/AivJ-A3v3EIXXnqAL; __Secure-3PAPISID=TA9nKw4_K1EqAc9-/AivJ-A3v3EIXXnqAL; SID=VwjeVSCSxO-Pz58nFkKXeAPWuSmX0RM1d225sJWDRrenH4cYHQBCEnFXDz4HkYFi84Di1g.; __Secure-1PSID=VwjeVSCSxO-Pz58nFkKXeAPWuSmX0RM1d225sJWDRrenH4cYXWoBmoLIhjwFQp0pIsYhUg.; __Secure-3PSID=VwjeVSCSxO-Pz58nFkKXeAPWuSmX0RM1d225sJWDRrenH4cYBn-4w6OM8MkLkkLD_i2JtA.; wide=1; LOGIN_INFO=AFmmF2swRQIgBPGFxAPDedjfS1LQmcZO9a2-Ptmwlj7HdekmLDycPokCIQDLPxPSXHfL0hzsvzLBmjIpQFZEmBNCKOaScgMFgbYr3A:QUQ3MjNmejk2ZDUxOUxWcjNIUWV1RGQ1S0lKdHl1OWdDLXRQdGtxQVNONEkyLUFHV0dJR0xacWRWRWpWdjFOY200OTdtWkJmaGpSdGt6QWhBcXgzRDE0aEpfbVlXYnVUUHhjV2FEanR6V2ZNaG1uQXZwU3g1WDQ5NExDTE0wajdvRTQ3WW13ZHVFc0N3SV9RTU5JbFNSNU1UdjRYT0FXQWxn; SIDCC=AP8dLtyQ_IIU4sxhjHC-RKAeVKNAkV4X-aaiB5PaYddWmJL67FH16rvKX1AEx_uybVoFlAhRc3rh; __Secure-1PSIDCC=AP8dLtyB7mlk9oAytlHNCfRI-SH-hNfhK0tyVm-j14LaQwNkU9kN3mAFgQE-5-TgItvbCVFQ62E; __Secure-3PSIDCC=AP8dLtxnJqQZAXyuUJeBFgwSuufL-rQ9jyApgYYFyxIWOeh4dtdtsqsyAZSZUoeENm5U43mqFl_-',
+	youtubeCookie: 'YSC=uh40_73-tck; VISITOR_INFO1_LIVE=6ZyLl2gRT1M; PREF=tz=America.Sao_Paulo&f6=40000000&f7=100; HSID=AYhG7c4P2o4KBknH7; SSID=A3nKJFovfd-VMKxk8; APISID=dnbXsVuYaF9qtxym/Aof3uYU1gEQY8b1rJ; SAPISID=9VqygLpBQV-A-w7P/AJA1RwJab5aBRt39j; __Secure-1PAPISID=9VqygLpBQV-A-w7P/AJA1RwJab5aBRt39j; __Secure-3PAPISID=9VqygLpBQV-A-w7P/AJA1RwJab5aBRt39j; SID=YwjeVdXu014SveujgWepG3BGL2aT-nurYU1JLnTzPf2ffM9wyv8TS2H-AbVzrXbFgAeEuQ.; __Secure-1PSID=YwjeVdXu014SveujgWepG3BGL2aT-nurYU1JLnTzPf2ffM9wbL0HjAIQYxoInVRCVTo4cw.; __Secure-3PSID=YwjeVdXu014SveujgWepG3BGL2aT-nurYU1JLnTzPf2ffM9w59f0v2zdQ0xM-KrDgvCiUA.; LOGIN_INFO=AFmmF2swRgIhAPdx6kvNK7va5CWoWYODTHawqE6Kr0UbCo4fzqfbEg-uAiEAp2tW4pi0zsbF-K5-JGv8vKngn1j-TvKpQH9Dq5Rvvgg:QUQ3MjNmeS13d1FLTkM3cU44Z0x0UGtoaktlbF9kQXNic1M4dVg5dXM3QU5wMDJOeFNzcE1QQmlzWFU3WEpFYjJ2VVZobTc4UGFUelFGaWlTYWt6TzFiUjdjYzFqTmQ3NHhVNGZpRFVyb2VtNURhSmUtQ3dyd0pROVdJRHRzZmduRGlweDFJOGRJMl9CSFJmUDVXd284eGlISHhkbVAwMDhB; wide=1; __Secure-1PSIDTS=sidts-CjIBPu3jIRJ8lIo8vgKUdayZiM1Mm3tHj_foCm1c880WDNxWfGP9daNpTfsAdlDhwenwthAA; __Secure-3PSIDTS=sidts-CjIBPu3jIRJ8lIo8vgKUdayZiM1Mm3tHj_foCm1c880WDNxWfGP9daNpTfsAdlDhwenwthAA; SIDCC=APoG2W-TN3BrQxrxi64B8ncM0TqhiFnsL3-2QA83yx0oTFngskdpqkLwxOj-fdW-U4Q9gA0vTVQ; __Secure-1PSIDCC=APoG2W-lvzwNRGXv-D729S7pObMPdaK_KItBH3syrJZW25SyVLTCfyDKhZgyQpNtiH-WcAenGCwd; __Secure-3PSIDCC=APoG2W9YTgLmMWWzaRVfvSPfVZyLQC99xe30FLgBto607El27Db8wPUyCgIuYppQ8-wh2KwkjM-x',
 });
 
 // Coleção de comandos
 client.commands = new Collection();
+
+/* Collection to store voice state */
+client.voiceManager = new Collection();
 
 export default client;
 
@@ -52,4 +56,5 @@ client.distube.on('error', (channel, e) => {
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN).then(() => {
 	loadCommands(client);
+	loadErrorHandler(client);
 });
