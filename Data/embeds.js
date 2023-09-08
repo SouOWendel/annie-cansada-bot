4de9abbbe322f9421e7c527bf0250749ba149561
Mark57449 <souowendel@gmail.com>
2023-09-08 01:29:33 -0300

    feat: criação do arquivo de embeds e adição de um embed para erros gerais
    Os embeds neste arquivo são exportados e podem ser resgatados para uso em qualquer arquivo que ele
    seja necessário

---
+++ Data/embeds.js
@@ -0,0 +1,12 @@
+export const generalErrorEmbed = {
+	'fields': [],
+	'title': '[MAGI System Error]',
+	'description': 'Ocorreu um erro, tente novamente. ',
+	'color': 11616063,
+	'footer': {
+		'text': 'Utilize o comando /feedback para informar um bug.',
+	},
+	'thumbnail': {
+		'url': 'https://media.tenor.com/3E4p3EslD1kAAAAj/terraria-sticker.gif',
+	},
+};
