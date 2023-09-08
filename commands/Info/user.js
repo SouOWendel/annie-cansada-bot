698e2d29cd87bf59492cafa52d57026eeef0b8dc
Mark57449 <souowendel@gmail.com>
2023-09-08 01:26:51 -0300

    feat(commands): adição de uma função que manda uma mensagem no chat privado da pessoa que interagir

--- commands/Info/user.js
+++ commands/Info/user.js
@@ -7,5 +7,6 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
 	// interaction.user is the object representing the User who ran the command
 	// interaction.member is the GuildMember object, which represents the user in the specific guild
+	interaction.user.send('Seu bobo. https://media.discordapp.net/attachments/381832868910989314/1103850128659468309/image.png');
 	await interaction.reply(`Esse comando foi enviado por ${interaction.user.username}, que entrou no servidor em ${interaction.member.joinedAt}.`);
 }
